8527f2313d0dc3711298b8a54d156f3d17eb2baf
Andrew Sanchez <druu831@gmail.com>
2023-03-21 19:45:31 -0700

    reverse-linked-list

---
+++ reverse-linked-list.js
@@ -0,0 +1,19 @@
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+// Example 1:
+// Input: head = [1,2,3,4,5]
+// Output: [5,4,3,2,1]
+
+// Solution #1 - Time(O(n)), Space(O(1))
+function reverseLinkedList(head) {
+  let curr = head;
+  let prev = null;
+  let next = null;
+
+  while (curr !== null) {
+    next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+  return prev;
+}
